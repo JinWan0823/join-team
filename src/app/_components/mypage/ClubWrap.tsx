120d39d7afbb261808d30d02c9d11e252c7198ab
jinWan0823 <dhksl823@gmail.com>
2024-02-28 23:34:16 +0900

    🎨 마이페이지 클럽목록 api 연결

--- src/app/_components/mypage/ClubWrap.tsx
+++ src/app/_components/mypage/ClubWrap.tsx
@@ -1,16 +1,30 @@
+"use client";
+import { useEffect, useState } from "react";
 import ListCard from "../main/ListCard";
+import { getData } from "@/app/_utils/axios";
+import { ClubData } from "../main/ListWrap";
 
 export default function ClubWrap() {
+  const [data, setData] = useState<ClubData[]>([]);
+  const url = "http://localhost:9999/club";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getData<ClubData[]>(url);
+        setData(result);
+      } catch (error) {
+        console.error("Data Fetching Error : ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
-    <>
-      <ul>
-        <ListCard />
-        <ListCard />
-        <ListCard />
-        <ListCard />
-        <ListCard />
-        <ListCard />
-      </ul>
-    </>
+    <ul>
+      {data.map((item, idx) => (
+        <ListCard key={idx} data={item} />
+      ))}
+    </ul>
   );
 }

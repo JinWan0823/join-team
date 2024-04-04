2ab4f14843318c14c931aebb2904864b22d3f90b
jinWan0823 <dhksl823@gmail.com>
2024-04-04 14:15:23 +0900

    🎨 마이페이지 피드/클럽 탭 병합 , 서버 요청 감소

--- src/app/_components/mypage/InfoTab.tsx
+++ src/app/_components/mypage/InfoTab.tsx
@@ -1,10 +1,34 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FeedWrap from "./FeedWrap";
 import ClubWrap from "./ClubWrap";
+import { getData } from "@/app/_utils/axios";
+import { joinTeamUrl } from "@/app/_utils/url";
+import { ClubDetailData, FeedData } from "@/app/_utils/Interface";
 
 export default function InfoTab() {
   const [toggleTab, setToggleTab] = useState(true);
+  const [feedData, setFeedData] = useState<FeedData[]>();
+  const [clubData, setClubData] = useState<ClubDetailData[]>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const feedResult = (await getData(
+          `${joinTeamUrl}/feed/myfeed`
+        )) as FeedData[];
+        const clubResult = (await getData(
+          `${joinTeamUrl}/club/myclub`
+        )) as ClubDetailData[];
+
+        setFeedData(feedResult);
+        setClubData(clubResult);
+      } catch (error) {
+        console.error("Data Fetching Error : ", error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -26,7 +50,11 @@ export default function InfoTab() {
           클럽
         </li>
       </ul>
-      {toggleTab ? <FeedWrap /> : <ClubWrap />}
+      {toggleTab ? (
+        <FeedWrap data={feedData as FeedData[]} />
+      ) : (
+        <ClubWrap data={clubData as ClubDetailData[]} />
+      )}
     </>
   );
 }

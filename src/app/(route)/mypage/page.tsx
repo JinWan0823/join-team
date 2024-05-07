62857363b14ac926f04f36f5f3e03fea346a1af7
jinWan0823 <dhksl823@gmail.com>
2024-05-07 16:24:46 +0900

    ⚡️ 쿠키 값 조회 후 렌더링

--- src/app/(route)/mypage/page.tsx
+++ src/app/(route)/mypage/page.tsx
@@ -1,51 +1,14 @@
-"use client";
-import InfoComment from "@/app/_components/mypage/InfoComment";
-import MyInfo from "@/app/_components/mypage/MyInfo";
-import InfoTab from "@/app/_components/mypage/InfoTab";
-import { useEffect, useState } from "react";
-import { getData } from "@/app/_utils/axios";
-import { UserData } from "@/app/_utils/Interface";
-import { IoIosSettings } from "react-icons/io";
-import Setting from "@/app/_components/mypage/Setting";
+import MypageWrap from "@/app/_components/mypage/MypageWrap";
+import { cookies } from "next/headers";
 
 export default function Wrap() {
-  const url = "http://localhost:8080/user";
-
-  const [userData, setUserData] = useState<UserData>();
-  const [settingMenu, setSettingMenu] = useState<boolean>(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await getData<UserData>(url);
-        setUserData(result);
-      } catch (err) {
-        throw err;
-      }
-    };
-    fetchData();
-  }, []);
+  const cookieStore = cookies();
+  const theme = cookieStore.get("connect.sid");
+  console.log(theme);
 
   return (
     <section className="max-h-[calc(100vh-66px)] h-[calc(100vh-66px)] overflow-y-auto bg-[#f1f1f1] pb-[56px] scroll-track relative">
-      <div className="p-[10px]">
-        {userData && <MyInfo user={userData} />}
-        {userData && <InfoComment comment={userData?.introComment} />}
-        <div
-          className="absolute right-[10px] top-[10px] cursor-pointer"
-          onClick={() => {
-            setSettingMenu(true);
-          }}
-        >
-          <IoIosSettings className="text-xl text-[#878787]" />
-        </div>
-
-        {/* <InterestReset /> */}
-      </div>
-      <InfoTab />
-      {settingMenu && userData && (
-        <Setting setSettingMenu={setSettingMenu} user={userData} />
-      )}
+      {theme?.value ? <MypageWrap /> : ""}
     </section>
   );
 }
---
+++ src/app/_components/mypage/MypageWrap.tsx
@@ -0,0 +1,52 @@
+"use client";
+import InfoComment from "@/app/_components/mypage/InfoComment";
+import MyInfo from "@/app/_components/mypage/MyInfo";
+import InfoTab from "@/app/_components/mypage/InfoTab";
+import { useEffect, useState } from "react";
+import { getData } from "@/app/_utils/axios";
+import { UserData } from "@/app/_utils/Interface";
+import { IoIosSettings } from "react-icons/io";
+import Setting from "@/app/_components/mypage/Setting";
+
+export default function MypageWrap() {
+  const url = "http://localhost:8080/user";
+
+  const [userData, setUserData] = useState<UserData>();
+  const [settingMenu, setSettingMenu] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getData<UserData>(url);
+        setUserData(result);
+      } catch (err) {
+        throw err;
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <>
+      <div className="p-[10px]">
+        {userData && <MyInfo user={userData} />}
+        {userData && <InfoComment comment={userData?.introComment} />}
+
+        <div
+          className="absolute right-[10px] top-[10px] cursor-pointer"
+          onClick={() => {
+            setSettingMenu(true);
+          }}
+        >
+          <IoIosSettings className="text-xl text-[#878787]" />
+        </div>
+
+        {/* <InterestReset /> */}
+      </div>
+      <InfoTab />
+      {settingMenu && userData && (
+        <Setting setSettingMenu={setSettingMenu} user={userData} />
+      )}
+    </>
+  );
+}

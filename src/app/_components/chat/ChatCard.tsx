b2f668ef31d77817e9ceeab128e9a63875d3bd09
jinWan0823 <dhksl823@gmail.com>
2024-07-26 20:47:33 +0900

    ✨ 유저가 가입한 클럽 채팅 조회 API 연결

--- src/app/_components/chat/ChatCard.tsx
+++ src/app/_components/chat/ChatCard.tsx
@@ -1,18 +1,38 @@
+import { ChatListData } from "@/app/_utils/Interface";
+import Image from "next/image";
+import Link from "next/link";
 import { FaUsers } from "react-icons/fa";
 
-export default function ChatCard() {
+interface DataProps {
+  data: ChatListData;
+}
+
+export default function ChatCard({ data }: DataProps) {
   return (
-    <li className="w-full px-[16px] py-[16px] hover:bg-[#dfdfdf] first:border-t-[1px] border-b-[1px] border-[#dfdfdf] flex-between items-center">
-      <div className="thumb w-[56px] h-[56px] rounded-[10px] bg-[#333]"></div>
-      <div className="w-[calc(100%-64px)]">
-        <h4 className="font-bold w-full">내향인들을 위한 러닝크루</h4>
-        <p className="font-bold pt-[2px] text-sm text-[#787878] flex items-center">
-          <span className="inline-flex items-center mr-[4px]">
-            <FaUsers /> 15
-          </span>
-          <span>오후 3:38</span>
-        </p>
-      </div>
+    <li>
+      <Link
+        className="w-full px-[16px] py-[16px] hover:bg-[#dfdfdf] first:border-t-[1px] border-b-[1px] border-[#dfdfdf] flex-between items-center"
+        href={"/chat"}
+      >
+        <div className="thumb w-[56px] h-[56px] rounded-[10px] bg-[#333] overflow-hidden">
+          <Image
+            src={data.thumb}
+            alt="club-thumbnail"
+            width={56}
+            height={56}
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="w-[calc(100%-64px)]">
+          <h4 className="font-bold w-full">{data.clubTitle}</h4>
+          <p className="font-bold pt-[2px] text-sm text-[#787878] flex items-center">
+            <span className="inline-flex items-center mr-[4px]">
+              <FaUsers /> {data.member.length}
+            </span>
+            <span>오후 3:38</span>
+          </p>
+        </div>
+      </Link>
     </li>
   );
 }
---
+++ src/app/_components/chat/ChatList.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import ChatCard from "@/app/_components/chat/ChatCard";
+import { getData } from "@/app/_utils/axios";
+import { ChatListData } from "@/app/_utils/Interface";
+import { joinTeamUrl } from "@/app/_utils/url";
+import { useEffect, useState } from "react";
+import { ImLab } from "react-icons/im";
+
+export default function ChatList() {
+  const [listData, setListData] = useState<ChatListData[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getData<ChatListData[]>(
+          `${joinTeamUrl}/chat/list`
+        );
+
+        setListData(result);
+      } catch (error) {
+        console.error("Initial Data Fetching Error: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <ul>
+      {listData.length > 0
+        ? listData.map((item, idx) => <ChatCard key={idx} data={item} />)
+        : "가입한 클럽이 없습니다."}
+    </ul>
+  );
+}

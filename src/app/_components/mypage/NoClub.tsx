1e373a2d88751b7cb04600e7c12daacc2b360cb6
jinWan0823 <dhksl823@gmail.com>
2024-07-30 18:09:43 +0900

    ✨웹소켓 설치 , 서버와 연결

--- src/app/_components/mypage/NoClub.tsx
+++ src/app/_components/mypage/NoClub.tsx
@@ -1,14 +1,11 @@
-import Image from "next/image";
 import Link from "next/link";
+import NoResultImg from "../common/NoResultImg";
 
 export default function NoClub() {
-  const noResult =
-    "https://jointeam.s3.ap-northeast-2.amazonaws.com/utill/Nosearch.avif";
-
   return (
     <>
       <div className="pt-[60px] flex flex-col justify-center items-center">
-        <Image src={noResult} width={320} height={320} alt="error-img" />
+        <NoResultImg />
         <Link
           href={"/search"}
           className="mt-[10px] bg-[#3D97FF] text-[#fff] px-[16px] py-[12px]  rounded-[10px] text-center"
--- src/app/_components/search/NoneSearchResult.tsx
+++ src/app/_components/search/NoneSearchResult.tsx
@@ -1,14 +1,10 @@
 import Link from "next/link";
-import { GoNote } from "react-icons/go";
-
+import NoResultImg from "../common/NoResultImg";
 export default function NoneSearchResult() {
   return (
     <>
       <div className=" flex flex-col justify-center items-center h-full">
-        <GoNote className="text-[60px] text-[#787878]" />
-        <p className="text-[#878787] font-bold mt-[10px]">
-          검색 결과가 없습니다.
-        </p>
+        <NoResultImg />
         <Link
           href={""}
           className="mt-[10px] bg-[#3D97FF] text-[#fff] py-[6px] w-[120px] rounded-[6px] text-center"

3f99e4f7772f46ca53b6f1aa18fbfaa67a3900e1
jinWan0823 <dhksl823@gmail.com>
2024-10-21 20:25:18 +0900

    ⚡️ 상대방 채팅 유저 썸네일,이름 렌더링

--- src/app/_components/chat/MyChat.tsx
+++ src/app/_components/chat/MyChat.tsx
@@ -8,9 +8,9 @@ export default function MyChat({ content, time }: ContentProps) {
     <>
       <div className="flex justify-end my-[10px] items-end">
         <p className="text-sm mr-[4px]">{time}</p>
-        <div className="my-chat bg-blue-200 p-[10px] rounded-[10px] max-w-[60%]">
+        <p className="my-chat bg-blue-200 p-[10px] rounded-[10px] max-w-[60%] break-words">
           {content}
-        </div>
+        </p>
       </div>
     </>
   );

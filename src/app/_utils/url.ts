d4500aa5b8adc588d6f1c963e00718ddcde544d3
jinWan0823 <dhksl823@gmail.com>
2024-12-18 02:02:38 +0900

    📝 환경 변수 적용, API URL 설정 수정

--- src/app/_utils/url.ts
+++ src/app/_utils/url.ts
@@ -1,7 +1,5 @@
 const defaultThumb =
   "https://jointeam.s3.ap-northeast-2.amazonaws.com/utill/defaultThumb.png";
 
-const joinTeamUrl =
-  // "http://jointeamserver-env.eba-mxsmsvyv.ap-northeast-2.elasticbeanstalk.com";
-  "http://localhost:8080";
+const joinTeamUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
 export { defaultThumb, joinTeamUrl };

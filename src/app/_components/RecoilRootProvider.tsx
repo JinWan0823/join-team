201b5e5ff8b136af325ee78631def101baace424
jinWan0823 <dhksl823@gmail.com>
2025-01-20 19:23:48 +0900

    ⚡️ 401 에러 처리 로직 개선: 전역 상태로 에러 관리 및 로그인 페이지 이동 기능 추가

--- src/app/_components/RecoilRootProvider.tsx
+++ src/app/_components/RecoilRootProvider.tsx
@@ -1,25 +1,10 @@
 "use client";
-import { RecoilRoot, useSetRecoilState } from "recoil";
-import { errorState } from "../_state/recoil";
-import { setErrorUpdater } from "../_utils/apiclient";
+import { RecoilRoot } from "recoil";
 
 export default function RecoilRootProvider({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
-  return (
-    <RecoilRoot>
-      <Initializer>{children}</Initializer>
-    </RecoilRoot>
-  );
-}
-
-function Initializer({ children }: { children: React.ReactNode }) {
-  const setError = useSetRecoilState(errorState);
-
-  // setErrorUpdater를 Recoil 상태를 초기화한 후에 실행
-  setErrorUpdater(setError);
-
-  return <>{children}</>;
+}>) {
+  return <RecoilRoot>{children}</RecoilRoot>;
 }
